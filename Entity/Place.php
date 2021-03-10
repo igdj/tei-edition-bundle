e116e44278a44d3458a06cfeaa6d7070d13b427a
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2021-03-10 17:26:33 +0100

    Add Place::mapAatToType($aatLabel)

--- Entity/Place.php
+++ Entity/Place.php
@@ -34,6 +34,85 @@ extends PlaceBase
         'inhabited place' => 10,
     ];
 
+    static $aatToType = [
+        'facets (controlled vocabulary)' => 'root', // http://vocab.getty.edu/aat/300386699
+        'continents' => 'continent',                // http://vocab.getty.edu/aat/300128176
+        'oceans' => 'ocean',                        // http://vocab.getty.edu/aat/300008687
+        'seas' => 'sea',                            // http://vocab.getty.edu/aat/300008694
+        'archipelagos' => 'archipelago',            // http://vocab.getty.edu/aat/300386854
+        'peninsulas' => 'peninsula',                // http://vocab.getty.edu/aat/300008804
+        'island groups' => 'island group',          // http://vocab.getty.edu/aat/300386853
+        'islands (landforms)' => 'island',          // http://vocab.getty.edu/aat/300008791
+        'gulfs (bodies of water)' => 'gulf',        // http://vocab.getty.edu/aat/300132315
+        'bays (bodies of water)' => 'bay',          // http://vocab.getty.edu/aat/300132316
+        'rivers' => 'river',                        // http://vocab.getty.edu/aat/300008707
+        'streams' => 'stream',                      // http://vocab.getty.edu/aat/300008699
+        'lakes (bodies of water)' => 'lake',        // http://vocab.getty.edu/aat/300008680
+        'mountains' => 'mountain',                  // http://vocab.getty.edu/aat/300008795
+        'mountain ranges' => 'mountain range',      // http://vocab.getty.edu/aat/300386831
+        'mountain systems' => 'mountain system',    // http://vocab.getty.edu/aat/300386832
+        'hills' => 'hill',                          // http://vocab.getty.edu/aat/300008777
+        'nation' => 'nations',                      // http://vocab.getty.edu/aat/300128207
+        'countries (sovereign states)' => 'country',// http://vocab.getty.edu/aat/300387506
+        'autonomous regions' => 'autonomous region',// http://vocab.getty.edu/aat/300387107
+        'autonomous republics' => 'autonomous republic',// http://vocab.getty.edu/aat/300387110
+        'dependent states' => 'dependent state',    // http://vocab.getty.edu/aat/300387176
+        'associations' => 'association',            // http://vocab.getty.edu/aat/300025950, e.g. Commonwealth
+        'semi-independent political entities' => 'semi-independent political entity',    // http://vocab.getty.edu/aat/300387052
+        'former groups of political entitites' => 'former group of political entitites',    // http://vocab.getty.edu/aat/300387354
+        'former primary political entities' => 'former primary political entity',    // http://vocab.getty.edu/aat/300387356
+        'former administrative divisions' => 'former administrative division',    // http://vocab.getty.edu/aat/300387179
+        'states (political divisions)' => 'state',  // http://vocab.getty.edu/aat/300000776
+        'first level subdivisions (political entities)' => 'province',  // http://vocab.getty.edu/aat/300387064
+        'third level subdivisions (political entities)' => 'district',  // http://vocab.getty.edu/aat/300387064
+        'cantons (administrative bodies)' => 'canton', // http://vocab.getty.edu/aat/300000769
+        'prefectures' => 'prefecture',              // http://vocab.getty.edu/aat/300235099
+        'governorates' => 'governorate',            // http://vocab.getty.edu/aat/300235093
+        'oblasts' => 'oblast',                      // http://vocab.getty.edu/aat/300235107
+        'voivodeships' => 'voivodeship',            // http://vocab.getty.edu/aat/300235112
+        'krays' => 'kray',                          // http://vocab.getty.edu/aat/300395501
+        'provinces' => 'province',                  // http://vocab.getty.edu/aat/300000774
+        'national districts' => 'national district',// http://vocab.getty.edu/aat/300387081
+        'metropolitan areas' => 'metropolitan area',// http://vocab.getty.edu/aat/300132618
+        'general regions' => 'general region',      // http://vocab.getty.edu/aat/300387346
+        'regions (administrative divisions)' => 'region', // http://vocab.getty.edu/aat/300236112
+        'regional divisions' => 'regional divisions', // http://vocab.getty.edu/aat/300387131
+        'historical regions' => 'historical region', // http://vocab.getty.edu/aat/300387178
+        'areas (geography)' => 'area',              // http://vocab.getty.edu/aat/300387575
+        'Bezirke' => 'district',                    // http://vocab.getty.edu/aat/300387333
+        'counties' => 'county',                     // http://vocab.getty.edu/aat/300000771
+        'duchies' => 'duchy',                       // http://vocab.getty.edu/aat/300235088
+        'autonomous cities' => 'autonomous city',   // http://vocab.getty.edu/aat/300387069
+        'independent cities' => 'independent city', // http://vocab.getty.edu/aat/300387068
+        'special cities' => 'special city',         // http://vocab.getty.edu/aat/300387067
+        'autonomous communities' => 'autonomous community', // http://vocab.getty.edu/aat/300387113
+        'municipalities' => 'municipality',         // http://vocab.getty.edu/aat/300265612
+        'special municipalities' => 'special municipality', // http://vocab.getty.edu/aat/300387213
+        'inhabited places' => 'inhabited place',    // http://vocab.getty.edu/aat/300008347
+        'suburbs' => 'suburb',                      // http://vocab.getty.edu/aat/300000874
+        'neighborhoods' => 'neighborhood',          // http://vocab.getty.edu/aat/300000745
+        'urban districts' => 'urban district',      // http://vocab.getty.edu/aat/300387337
+        'Ortsteile' => 'city district',             // http://vocab.getty.edu/aat/300387337
+        'boroughs' => 'borough',                    // http://vocab.getty.edu/aat/300000778
+        'unitary authorities' => 'unitary authority', // http://vocab.getty.edu/aat/300387071
+        'deserted settlements' => 'deserted settlement', // http://vocab.getty.edu/aat/300167671
+        'historic sites' => 'historic site',        // http://vocab.getty.edu/aat/300000833
+        'castles (fortifications)' => 'castle',     // http://vocab.getty.edu/aat/300006891
+        'miscellaneous' => 'miscellaneous',         // http://vocab.getty.edu/aat/300386698
+    ];
+
+    // Getty returns plurals for placeTypePreferred
+    // e.g. continents for http://vocab.getty.edu/aat/300128176
+    // we decided to store the singular form, therefore we need to map
+    public static function mapAatToType($aatLabel)
+    {
+        if (array_key_exists($aatLabel, self::$aatToType)) {
+            return self::$aatToType[$aatLabel];
+        }
+
+        return null;
+    }
+
     public static function buildTypeLabel($type)
     {
         if ('root' == $type) {
